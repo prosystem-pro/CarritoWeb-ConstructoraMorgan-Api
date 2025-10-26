a262090c91db6d77ee1904bc3050cc2683ac9955
prosystem-pro <prosystem490@gmail.com>
2025-10-26 17:20:13 -0600

    Despliegue inicial Api Constructora Morgan

---
+++ src/FuncionIntermedia/VerificarPermisos.js
@@ -0,0 +1,30 @@
+const { ObtenerPermisosPorRolYRecurso } = require('../Servicios/PermisoPorRolRecursoServicio');
+const ManejarError = require('../Utilidades/ErrorControladores');
+
+const VerificarPermisos = (Permiso, Recurso) => {
+  return async (req, res, next) => {
+    try {
+      const { CodigoRol, SuperAdmin } = req.Datos;
+
+      if (SuperAdmin === 1) {
+        return next();
+      }
+z
+      if (!CodigoRol) {
+        return res.status(403).json({ error: 'No autorizado, rol no proporcionado' });
+      }
+
+      const Permisos = await ObtenerPermisosPorRolYRecurso(CodigoRol, Recurso, Permiso);
+
+      // if (!Permisos.includes(Permiso)) {
+      //   LanzarError('No tienes permiso para realizar esta acción.', 403, 'Alerta');
+      // }
+
+      next();
+    } catch (error) {
+      ManejarError(error, res, 'Error verificando permisos');
+    }
+  };
+};
+
+module.exports = VerificarPermisos;
